427259df53f4a49c7c269259fe36bea61d26002b
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2024-06-26 16:11:00 +0300

    v1

---
+++ index.js
@@ -0,0 +1,93 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import multer, { diskStorage, MulterError } from "multer";
+import { extname as _extname } from "path";
+import * as WorkerController from "./controllers/WorkersController.js";
+
+// mongoose
+//   .connect(
+//     "mongodb+srv://investvesko:psOaRrCJ813mnSYD@cluster0.qxpgtnc.mongodb.net/phone_security?retryWrites=true&w=majority"
+//   )
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("DB is ok");
+  })
+  .catch((e) => {
+    console.log("DB is error", e);
+  });
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+const PORT = process.env.PORT || 3000;
+
+// Настройка хранилища для Multer
+const storage = diskStorage({
+  destination: (req, file, cb) => {
+    // Укажите директорию для загрузки файлов
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    // Переименуйте файл для предотвращения конфликтов имен
+    cb(null, file.fieldname + "-" + Date.now() + _extname(file.originalname));
+  },
+});
+
+// Инициализация Multer с указанным хранилищем и ограничениями
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // Ограничение на размер файла 5MB
+  fileFilter: (req, file, cb) => {
+    // Разрешение только на загрузку изображений
+    const filetypes = /jpeg|jpg|png|gif/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(_extname(file.originalname).toLowerCase());
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    } else {
+      cb(new Error("Only images are allowed!"));
+    }
+  },
+});
+
+// Маршрут для загрузки файла
+app.post("/upload", upload.single("image"), (req, res) => {
+  if (req.file) {
+    res.status(200).json({
+      message: "Файл загружен успешно!",
+      file: req.file,
+    });
+  } else {
+    res.status(400).json({
+      message: "Ошибка при загрузке файла.",
+    });
+  }
+});
+
+// Обработка ошибок Multer
+app.use((err, req, res, next) => {
+  if (err instanceof MulterError) {
+    res.status(500).json({ message: err.message });
+  } else if (err) {
+    res.status(500).json({ message: err.message });
+  } else {
+    next();
+  }
+});
+
+app.post("/worker", WorkerController.create);
+app.post("/worker/:id/gadgets", WorkerController.addGadget);
+app.delete("/worker/:id/gadgets/:gadgetId", WorkerController.removeGadget);
+app.get("/worker", WorkerController.getAll);
+app.get("/worker/:id", WorkerController.getOne);
+app.delete("/worker/:id", WorkerController.removeWorker);
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
---
+++ models/Worker.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const gadgetSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  brand: { type: String, required: true },
+  model: { type: String, required: true },
+  sn: { type: String, required: true, unique: true },
+  imgUrl: { type: String, required: false },
+  purchasedOn: { type: Date, default: Date.now },
+});
+
+const WorkerSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  position: { type: String, required: false },
+  imgUrl: { type: String, required: false },
+  gadgets: [gadgetSchema],
+});
+
+export default mongoose.model("Worker", WorkerSchema);
